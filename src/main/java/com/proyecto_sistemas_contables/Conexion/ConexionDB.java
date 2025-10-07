f175275c28d13e2a1e1b9c4921c6b73d7269e064
JColocho <Jcolocho2003@gmail.com>
2025-10-06 21:21:14 -0600

    Estructura inicial del proyecto

---
+++ src/main/java/com/proyecto_sistemas_contables/Conexion/ConexionDB.java
@@ -0,0 +1,21 @@
+package com.proyecto_sistemas_contables.Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    private static final String url = "jdbc:postgresql://localhost:5432/db_sistema_contable";
+    private static final String USER = "postgres";
+    private static final String PASS = "12345678";
+
+    public static Connection connection(){
+        try {
+            Connection conectar = DriverManager.getConnection(url,USER,PASS);
+            System.out.println("Conectado a la base");
+            return conectar;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
